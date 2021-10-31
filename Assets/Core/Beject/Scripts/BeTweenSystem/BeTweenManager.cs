93d8ed6639dfccd07bf92bf734b96f6914ffe03e
Danil <clichindustries@gmail.com>
2021-10-31 16:23:09 +0700

    Start Work

---
+++ Assets/Core/Beject/Scripts/BeTweenSystem/BeTweenManager.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+namespace MBSCore.BeTweenSystem
+{
+    public class BeTweenManager : MonoBehaviour, IBeTweenManager
+    {
+        public int CreateScaledTween()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public int CreateUnscaledTween()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void PlayTween(int tweenId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool IsPlayed(int tweenId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void StopTween(int tweenId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private int CalculateTweenId()
+        {
+            return -1;
+        }
+        
+        private enum TweenType
+        {
+            None = 0,
+            Scaled = 1,
+            UnScaled = 2,
+        }
+    }
+}
---
+++ Assets/Core/Beject/Scripts/BeTweenSystem/IBeTweenManager.cs
@@ -0,0 +1,11 @@
+﻿namespace MBSCore.BeTweenSystem
+{
+    public interface IBeTweenManager
+    {
+        int CreateScaledTween();
+        int CreateUnscaledTween();
+        void PlayTween(int tweenId);
+        bool IsPlayed(int tweenId);
+        void StopTween(int tweenId);
+    }
+}
